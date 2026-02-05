13ba7e7691e646711f1d2a879373dc4ca82c403f
Shida78 <anton-kulish15@mail.ru>
2026-02-05 14:08:31 +0700

    home page-bd count

--- gallery/views.py
+++ gallery/views.py
@@ -3,8 +3,16 @@ from django.http import HttpResponse
 
 # request — это "письмо" от браузера с данными о пользователе
 def home(request):
-    # Мы пока не используем HTML-шаблоны, просто вернем строку.
-    return HttpResponse("<h1>Добро пожаловать в 3D Хранилище</h1><p>Система работает.</p>")
+    # 1. Готовим данные (Context). Это словарь Python.
+    # Ключи словаря станут именами переменных в HTML.
+    context_data = {
+    'page_title': 'Главная Галерея',
+    'models_count': 0, # Попробуйте поменять на 5, чтобы проверить условие
+    }
+
+    # 2. Рендерим (смешиваем HTML и данные)
+    # Путь указываем относительно папки templates: 'gallery/index.html'
+    return render(request, 'gallery/index.html', context_data)
 
 def about(request):
     # Мы пока не используем HTML-шаблоны, просто вернем строку.
