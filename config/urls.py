d448a41e324a74eb25581249559e8d321e3854f7
Shida78 <anton-kulish15@mail.ru>
2026-02-18 22:13:42 +0700

    5 practice without dop

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 # Импортируем нашу функцию из приложения gallery
 from gallery.views import home
 from gallery.views import about
+from gallery.views import upload
 
 # Импорты для настройки медиа
 from django.conf import settings
@@ -30,6 +31,7 @@ urlpatterns = [
     # Пустая строка '' означает главную страницу сайта (http://localhost:8000/)
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('upload/', upload, name='upload'),
 ]
 # ВНИМАНИЕ: Эта магия работает только если DEBUG = True (режим разработки)
 if settings.DEBUG:
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-from .models import Asset # Импортируем модель, чтобы спрашивать данные
+from django.shortcuts import render, redirect # Добавляем redirect
+from .models import Asset
+from .forms import AssetForm # Импортируем нашу новую форму
+
+def upload(request):
+    if request.method == 'POST':
+        # Сценарий: Пользователь нажал "Отправить"
+        # ВАЖНО: Передаем request.FILES, иначе файл потеряется!
+        form = AssetForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # Если все поля заполнены верно - сохраняем в БД
+            form.save()
+            # И перекидываем пользователя на главную
+            return redirect('home')
+    else:
+        # Сценарий: Пользователь просто зашел на страницу (GET)
+        form = AssetForm() # Создаем пустую форму
+
+    # Отдаем шаблон, передавая туда форму (заполненную ошибками или пустую)
+    return render(request, 'gallery/upload.html', {'form': form})
 
 # request — это "письмо" от браузера с данными о пользователе
 def home(request):
@@ -23,9 +42,3 @@ def about(request):
     # Мы пока не используем HTML-шаблоны, просто вернем строку.
     #return HttpResponse("<h1>Курс Web Структуры</h1><p>Здесь мы делаем сайт типо Scetchfab.</p>")
     return render(request, 'gallery/about.html', context_data)
-
-def upload(request):
-    context_data = {
-    'page_title': 'Страница загрузки',
-    }
-    return render(request, 'gallery/upload.html', context_data)
